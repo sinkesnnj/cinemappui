93031d52b3ae18e42ebd7f94b15fea5c2a1714ec
Siniša Ilibašić <sinkesnnj@gmail.com>
2020-09-03 11:45:15 +0200

    Added production build logic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  token_auth_config: {
+    apiBase: 'https://cinemaappp.herokuapp.com',
+    signInPath: 'auth/sign_in',
+    signInRedirect: '/dashboard',
+
+    globalOptions: {
+      headers: {
+          'Content-Type':     'application/json',
+          'Accept':           'application/json'
+      }
+  }
+  }
 };
