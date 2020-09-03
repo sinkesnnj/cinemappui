b3d685c74d3e1115add70c402b604bc27ea4adc0
Siniša Ilibašić <sinkesnnj@gmail.com>
2020-09-03 12:00:34 +0200

    Added more stuff to run on heroku

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./dist/angie'));
+app.get('/*', function(req, res) { res.sendFile('index.html', {root: 'dist/angie'});});
+app.listen(process.env.PORT || 8080);
