515cdd0107f420521ef850f2317532c3c458cbdf
Siniša Ilibašić <sinkesnnj@gmail.com>
2020-09-04 10:42:56 +0200

    Fix for registration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,9 @@ export const environment = {
     apiBase: 'https://cinemaappp.herokuapp.com',
     signInPath: 'auth/sign_in',
     signInRedirect: '/dashboard',
+    registerAccountPath:        'auth',
+    deleteAccountPath:          'auth',
+    registerAccountCallback:    window.location.href,
 
     globalOptions: {
       headers: {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,9 @@ export const environment = {
     apiBase: 'https://cinemaappp.herokuapp.com',
     signInPath: 'auth/sign_in',
     signInRedirect: '/dashboard',
+    registerAccountPath:        'auth',
+    deleteAccountPath:          'auth',
+    registerAccountCallback:    window.location.href,
 
     globalOptions: {
       headers: {
